c735d850a3d3372df723352318d4ebedca2d78a6
Mateus Souza <mateus_msouza@outlook.com>
2021-08-15 14:15:51 -0300

    finish wl algo first implementation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from service_graph import ServiceGraph
 from wl_algo import run_wl
 
-if __name__ == '__main__':
-    graph_one = ServiceGraph().create(
+def main():
+    service_graph = ServiceGraph()
+    
+    graph_one = service_graph.create(
         vertex_amount=4,
         edges=[
             [0,1],
@@ -10,11 +12,22 @@ if __name__ == '__main__':
         ]
     )
     
-    graph_two = ServiceGraph().create(
+    graph_two = service_graph.create(
         vertex_amount=4,
         edges=[
             [3,2],
             [0,1]
         ]
-    )    
-    run_wl(graph_one, graph_two)
+    )
+    
+    seems_isomorphic = run_wl(graph_one, graph_two)
+    if seems_isomorphic:
+        print('those looks like isomorphic graphs')
+    else:
+        print('they arent isomorphic')
+
+    service_graph.plot_graph(graph_one)
+    service_graph.plot_graph(graph_two)
+
+if __name__ == '__main__':
+    main()
--- wl_algo.py
+++ wl_algo.py
@@ -1,26 +1,97 @@
 from igraph import *
 
-def run_wl(graph_one: Graph.Tree, graph_two: Graph.Tree) -> bool:
+def run_wl(graph_one: Graph, graph_two: Graph) -> bool:
+    """ run wl algo
+    
+    Args:
+        graph_one (Graph): first graph
+        graph_two (Graph): second graph
+
+    Returns:
+        bool: maybe these graphics are isomorphic
+    """    
     
     __set_all_as_one(graph_one)
     __set_all_as_one(graph_two)
     
-    for i in range(graph_one.vcount()):
-        nodeg1 = graph_one.vs[i]
-        nodeg2 = graph_one.vs[i]
-        
-        nodeg1['L'] = {
-            'label': nodeg1['name'],
-            'multiset': graph_one.neighbors(nodeg1)
-        }
-        
-        nodeg2['L'] = {
-            'label': nodeg2['name'],
-            'multiset': graph_one.neighbors(nodeg2)
-        }
-        print(nodeg1['L'])
-        print(nodeg2['L'])
+    recently_setted_as_one = True
+    
+    graph_one_vs_count = graph_one.vcount()
 
-def __set_all_as_one(graph: Graph.Tree) -> Graph.Tree:
+    for i in range(graph_one_vs_count):
+
+        for j in range(graph_one_vs_count):
+            nodeg1 = graph_one.vs[j]
+            nodeg2 = graph_two.vs[j]
+
+            nodeg1['metadata'] = {
+                'multiset': [
+                    graph_one.vs[x]['name'] for x in graph_one.neighbors(nodeg1)
+                ]
+            }
+            
+            nodeg2['metadata'] = {
+                'multiset': [
+                    graph_two.vs[x]['name'] for x in graph_two.neighbors(nodeg2)
+                ]
+            }
+
+        multiset_one, multiset_two = __gather_multisets(graph_one, graph_two)
+
+        if multiset_one == multiset_two and not recently_setted_as_one:
+            return True
+        __recolor_vertex(graph_one)
+        __recolor_vertex(graph_two)
+        recently_setted_as_one = False
+
+    return False
+            
+        
+def __gather_multisets(graph_one: Graph, graph_two: Graph) -> (list, list):
+    """Gather all vertex multisets in a graph in a list for comparision
+    purposes.
+    
+    Args:
+        graph_one (Graph): first graph
+        graph_two (Graph): second graph
+
+    Returns:
+        (list, list): sorted multiset list from graph one and sorted multiset list from graph two
+    """
+
+    multiset_list_one = []
+    multiset_list_two = []
+
+    for i in range(graph_one.vcount()):
+        node_one = graph_one.vs[i]
+        node_two = graph_two.vs[i]
+
+        multiset_list_one.append(node_one['metadata']['multiset'])
+        multiset_list_two.append(node_two['metadata']['multiset'])
+
+    return sorted(multiset_list_one), sorted(multiset_list_two)
+
+def __recolor_vertex(graph: Graph):
+    """recolor each vertex to have their hashed multiset as their new label
+
+    Args:
+        graph (Graph): graph to be recolored
+    """
+
+    for i in range(graph.vcount()):
+        node = graph.vs[i]
+        multiset = node['metadata']['multiset']
+        node['name'] = abs(hash(str(multiset)))
+
+def __set_all_as_one(graph: Graph) -> None:
+    """set all vertex to have label 1 to start first iteration.
+    that's inspired in this implementation https://davidbieber.com/post/2019-05-10-weisfeiler-lehman-isomorphism-test/
+
+    Args:
+        graph (Graph): graph to be setted to all 1.
+
+    Returns:
+        None
+    """    
     for node in graph.vs:
         node['name'] = 1
